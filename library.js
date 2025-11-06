3b233a6d5a015dbac002eb04c026a8929353c0e2
Snow <bis23-smsumba@mubas.ac.mw>
2025-11-06 22:55:58 +0200

    added some html and css rules

--- library.js
+++ library.js
@@ -1,8 +1,12 @@
-
-const button=document.getElementById("Addbook");
-
+const main=document.getElementById("main");
+const checkBook=document.getElementById("veiwbook");
+const books=document.getElementById("books")
+const Title=document.getElementById("title");
+const Author=document.getElementById("author");
+const Pages=document.getElementById("pages");
+const Read=document.getElementById("read");
+const Add=document.getElementById("Add");
 const mylibrary=[];
-
 function Book(title,author,pages,read){
     this.title=title;
     this.author=author;
@@ -10,17 +14,45 @@ function Book(title,author,pages,read){
     this.read=read;
 }
 
-function addbook(title,author,pages,read){
-  this.title= title;
-   this.author=author;
-   this.pages=pages;
-   this.read=read;
+function addbook(){
 
-    const book=new Book("Love","snow",126,"yes");
-    const book2=new Book("My love", "Snow", 100, "yes");
-    mylibrary.push(book,book2);
+       let book=new Book(Title.value,Author.value,Pages.value,Read.value);
+       mylibrary.push(book);
+       added=true
+       Title.value=""
+       Author.value=""
+       Pages.value=""
+       Read.value=""
 }
-addbook();
+function veiwbooks(){
+    books.innerHTML="";
 for (let i=0; i<mylibrary.length; i++){
-    console.log(mylibrary[i]);
+    const book=document.createElement('div');
+    const details=document.createElement('h2');
+    details.setAttribute('class','details');
+    book.setAttribute("class","mybook");
+    details.innerText=`Title: ${mylibrary[i].title}
+    Author: ${mylibrary[i].author}
+    Pages: ${mylibrary[i].pages}
+    Read Status: ${mylibrary[i].read}
+    `
+    books.style.cssText=`
+    display: grid;
+    grid-template-columns:1fr 1fr ;
+    grid-template-rows: 1fr 1f;
+    gap:2rem;
+    `
+    books.appendChild(book)
+    book.append(details);
 }
+}
+checkBook.addEventListener("click",veiwbooks,);
+
+Add.addEventListener("click",()=>{
+    if(Title.value!=""&&Pages.value!=""&&Author.value!=""&&Read.value!="")
+        addbook();
+    else
+        window.alert("please enter the details first")
+})
+
+
